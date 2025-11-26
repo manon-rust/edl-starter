e436a9286e89ce5193cb06ce630b094dd4f04005
manon-rust <manonrustin21@gmail.com>
2025-11-26 10:02:26 +0100

    feat: split unit and E2E tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -30,3 +30,10 @@ def client():
     """
     with TestClient(app) as test_client:
         yield test_client
+
+def pytest_configure(config):
+ """Enregistre les markers personnalisés"""
+ config.addinivalue_line(
+ "markers",
+ "e2e: mark test as end-to-end test (slow)"
+ )
